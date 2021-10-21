efab6cbf605c4d39afe787c9b02b3d5537d723f1
jamescito <jr76407900@gmail.com>
2021-10-21 14:55:53 -0600

    completamos la clase figura

---
+++ src/main/java/org/james/Figuras.java
@@ -0,0 +1,13 @@
+package org.james;
+import java.awt.*;
+public abstract class Figuras {
+
+        int lado = 0;
+        public Figuras(int lado) {
+            this.lado = lado;
+        }
+        public abstract double getArea();
+        public abstract void dibujar(Graphics g, int x, int y);
+    
+    }
+    
