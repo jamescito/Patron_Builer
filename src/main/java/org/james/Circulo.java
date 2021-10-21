d863654af24e8295d9aa3cb9fab778ba54ceaec0
jamescito <jr76407900@gmail.com>
2021-10-21 14:57:35 -0600

    Modificamos las clases pero la terminamos

--- src/main/java/org/james/Circulo.java
+++ src/main/java/org/james/Circulo.java
@@ -1,5 +1,19 @@
 package org.james;
+import java.awt.*;
 
-public class Circulo {
-    
+
+public class Circulo extends Figuras {
+
+    public Circulo(int lado){
+        super(lado);
+    }
+
+    public double getArea(){
+        return (Math.PI * lado * lado);
+    }
+
+    public void dibujar(Graphics g,int x, int y){
+        g.setColor(Color.BLACK);
+        g.drawArc(x, y, lado, lado, 0, 360);
+    }
 }
--- src/main/java/org/james/Cuadrado.java
+++ src/main/java/org/james/Cuadrado.java
@@ -1,5 +1,20 @@
 package org.james;
 
-public class Cuadrado {
-    
+import java.awt.*;
+
+public class Cuadrado extends Figuras {
+
+    public Cuadrado(int lado){
+        super(lado);
+    }
+
+    public double getArea(){
+        return (lado * lado);
+    }
+
+    public void dibujar(Graphics g, int x, int y){
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, lado, lado);
+    }
 }
+
